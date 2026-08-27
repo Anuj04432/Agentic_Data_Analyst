9ccd30c2748f93adb76c8aee1370c593debcdd04
Anuj04432 <anujwagmore835@gmail.com>
2026-08-28 02:15:25 +0530

    Tested all the functions in the ui

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
 
-from data_analysis.data import data_a
+from data.open_dataset import dataset_format
+from data.data_extract import data_info
 
 file = st.file_uploader(label="Upload your businees dataset",type=["csv","excel","sql"],max_upload_size=500,help="Upload the files that are mentioned",label_visibility="visible")
 if file is not None:
-    info = data_a(file)
-    col1,col2 = st.columns(2)
-    rows = info["shape"]
-    with col1:
-        st.metric(label="Rows",value=rows[0])
-    with col2:
-            st.metric(label="Columns",value=rows[1])
-    st.write(info["columns"])
+    filename = file.name
+    df = dataset_format(file,filename)
+    df = data_info(df)
+
+    st.write(df["columns"])
+    st.write(df["dtypes"])
+    st.write(df["sum1"])
+    st.write(df["sum"])
+    st.write("Null values are in the dataset",df["nullvalues"])
+    st.metric(label="rows",value=df["no_rows"])
+    
+   
+
 
 else:
     st.warning("Please upload the file first")
