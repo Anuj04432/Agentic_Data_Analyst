e44496275feb7066f3ddfad6961c5be619bce3a3
Anuj04432 <anujwagmore835@gmail.com>
2026-09-19 22:11:13 +0530

    add: function for formating numbers into strings with decimals

--- utils/helpers.py
+++ utils/helpers.py
@@ -10,4 +10,13 @@ def format_bytes(size_bytes: int | float) -> str:
             return f"{size_bytes:.2f} {unit}" if unit != "B" else f"{int(size_bytes)} B"
         size_bytes /= 1024.0
 
-    return f"{size_bytes}"
+    return f"{size_bytes:.2f} PB"
+
+def format_number(val: int | float | None, decimal: int = 2) -> str:
+    if val is None or pd.isna(val):
+        return "N/A"
+
+    if isinstance(val, (int,)):
+        return f"{val:,}"
+
+    return f"{val:,.{decimal}f}"
