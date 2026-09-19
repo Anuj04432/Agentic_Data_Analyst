b422ea800b9c47abd1b926f256e1e6f37be09b07
Anuj04432 <anujwagmore835@gmail.com>
2026-09-19 22:16:32 +0530

    add: function for formatting percentage

--- utils/helpers.py
+++ utils/helpers.py
@@ -12,11 +12,16 @@ def format_bytes(size_bytes: int | float) -> str:
 
     return f"{size_bytes:.2f} PB"
 
-def format_number(val: int | float | None, decimal: int = 2) -> str:
+def format_number(val: int | float | None, precision: int = 2) -> str:
     if val is None or pd.isna(val):
         return "N/A"
 
     if isinstance(val, (int,)):
         return f"{val:,}"
 
-    return f"{val:,.{decimal}f}"
+    return f"{val:,.{precision}f}"
+
+def format_percentage(val: float |None, precision: int = 1) -> str:
+    if val is None or pd.isna(val):
+        return "N/A"
+    return f"{val:.{precision}f}%"
